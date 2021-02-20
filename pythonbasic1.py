6b72166d1cd162b94ae09eee9de535405e9021d1
kaydenmok <kaydenmokrytzki@gmail.com>
2021-02-20 13:51:35 -0500

    finish number 1

--- pythonbasic1.py
+++ pythonbasic1.py
@@ -10,3 +10,5 @@
 # 		Like a diamond in the sky. 
 # Twinkle, twinkle, little star, 
 # 	How I wonder what you are
+
+print ("Twinkle, twinkle, little star,\n    How I wonder what you are!\n        Up above the world so high,\n       Like a diamond in the sky.\nTwinkle, twinkle, little star,\n    How I wonder what you are")
