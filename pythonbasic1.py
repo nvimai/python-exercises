66b4e68b0d925d56def6398961126df81d22f7a8
Nvi Mai <contact@nvimai.com>
2021-02-20 14:40:18 -0500

    updated questions

--- pythonbasic1.py
+++ pythonbasic1.py
@@ -37,3 +37,13 @@ def circleArea(radius):
 area = circleArea(radius)
 
 print("r = {0}\n area = {1}".format(radius, area))
+
+
+# 5. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them. 
+
+
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. 
+# Sample data : 3, 5, 7, 23
+# Output :
+# List : ['3', ' 5', ' 7', ' 23']
+# Tuple : ('3', ' 5', ' 7', ' 23')
