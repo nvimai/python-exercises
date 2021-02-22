b34849a749b5aba50c4311d7123097772168fa26
kaydenmok <kaydenmokrytzki@gmail.com>
2021-02-21 19:18:40 -0500

    d

--- pythonbasic1.py
+++ pythonbasic1.py
@@ -47,25 +47,79 @@
 # 12. Write a Python program to print the calendar of a given month and year.
 # Note : Use 'calendar' module.
 
-import calendar
-y = int(input("Input the year:"))
-m = int(input("Input the month:"))
-print(calendar.month(y,m))
+# import calendar
+# y = int(input("Input the year:"))
+# m = int(input("Input the month:"))
+# print(calendar.month(y,m))
 
 # 8. Write a Python program to display the first and last colors (First and last elements) from the following list. 
 # color_list = ["Red","Green","White" ,"Black"]
 
+# color_list = ["Red","Green","White", "Black"]
+
+# res = [color_list[0], color_list[-1]]
+
+# print ("The first and last colors are:" + str(res))
+
 # 113. Write a Python function to input a number, if it is not a number generate an error message, or is positive, negative or zero. 
 
+# def numberType():
+#     num = float(input("Enter a number: "))
+#     if num > 0:
+#         print("Your number is Positive")
+#     elif num == 0:
+#         print("Zero")
+#     else:
+#         print("Your number in negative")
+
+# numberType() 
+
 # 48. Write a Python function to parse a string to Float or Integer. Name the function as "string2Float" and "string2Int"
+
 
 # 34. Write a Python function to sum of two given (input) integers. However, if the sum is between 15 to 20 it will return 20. 
 
 # 65. Write a Python function to convert seconds to day, hour, minutes and seconds. Function name "seconds2Datetime"
 
+# def seconds2Datetime():
+
+#     time = float(input("Enter time in seconds:"))
+#     day = time // (24 * 3600)
+#     time = time % (24 * 3600)
+#     hour = time // 3600
+#     time %= 3600
+#     minutes = time // 60
+#     time %= 60
+#     seconds = time
+#     print("d:h:m:s -> %d: %d: %d: %d" % (day, hour, minutes, seconds)) 
+
+# seconds2Datetime()
+
 # 91. Write a Python function to swap two variables. Function name "swap2Vars"
 
+# def swap2Vars():
+#     x = 10
+#     y = 4
+
+#     temp = x
+#     x = y
+#     y = temp
+
+#     print("The value of x after swaping would be: {}".format(x))
+#     print("The value of y after swaping would be: {}".format(y))
+
+# swap2Vars()
+
 # 138. Write a Python function to convert true to 1 and false to 0. 
+def true1False0():
+    x = "true"
+    x = int(x == "true")
+    print(x)
+    x = "abcd"
+    x = int(x == "true")
+    print(x)
+
+true1False0()
 
 ##### Bonus:
 # 58. Write a python function to find the sum of the first n positive integers. 
