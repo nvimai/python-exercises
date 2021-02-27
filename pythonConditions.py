d5c8d2cbf97854c21df62e0f93681ff6b1c53f97
Nvi Mai <contact@nvimai.com>
2021-02-27 17:56:14 -0500

    Conditions, string, list and raspberry questions.

---
+++ pythonConditions.py
@@ -0,0 +1,44 @@
+# Finish 1 to 7 to have $7; 8 and 9 to have $8
+
+# 1. Write Python program to check a given list is empty or not. If not then return the length of that list 
+
+
+# 2. Write Python program to guess the Ontario school grades of a person by year of birth
+# Input: 2010
+# Output: Grade 5 in Elementary
+
+# 3. Write Python program to find the longest word and the length of it from the string1
+# Input: 
+#   string1: "Esse cillum quis ullamco est irure ipsum. Adipisicing ex non ullamco ipsum reprehenderits consequat sint irure proident nisi et adipisicing ut proident."
+# Output (example):
+#   Longest word: reprehenderits
+#   Length: 14
+
+
+# 4. Write Python program to replace string2 by "%%" from string1
+# Input: 
+#   string1: "Esse cillum quis ullamco est irure ipsum. Adipisicing ex non ullamco ipsum reprehenderits consequat sint irure proident nisi et adipisicing ut proident."
+#   string2: given from input
+
+# Output: The new string1 after replaced
+
+# 5. Write Python program to return a right Money Format of a given number
+# Input: 4235.254
+# Output: $4,235.25
+
+# 6. Write Python program to find the second longest digit of an element from an list.
+# Input: [123, 13432, 5452, 125, 67]
+# Output: The second longest digit number is '5452', which has 4 digits
+
+# 7. Write Python program to convert a given input to KB
+# Input: 1MB
+# Output: 1024KB
+
+# =====================
+# Raspberry Pi: 
+# 8. Based on the lesson 3 and use a press button to change the state when pressing n times:
+#   1. Press 1st time to turn on the flowing water light and make the flow running from left to right
+#   2. Press 2nd time to change the flow from right to left
+#   3. Press 3rd time to turn off the lights
+
+# 9. Do the Lesson 5: RGBLED
