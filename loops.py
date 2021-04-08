45cb9344edee4be03daab6f67367b18cec567306
Nvi Mai <nvi.mai@expogain.com>
2021-04-08 18:48:37 -0400

    loops

---
+++ loops.py
@@ -0,0 +1,27 @@
+# Done all following questions to have $10
+
+#1. Write a Python function to reverse the order of the items from an array
+
+#2. Write a Python function to sort an array by ascending or descending
+
+#3. Write a Python function to return the appearance of a specified element in an array
+
+#4. Write a Python function to sort a dictionary on value
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+#5. Write a Python function to remove all the occurrences of a specified item from an array
+
+#6. Write a Python function to sum all the items in an array of numbers
+
+#7. Write a Python function to sum all the items' value from a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+#8. Write a Python function to get the maximum and minimum value in a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+#9. Write a Python function to merge 2 dictionaries
+# d1 = {'a': 100, 'b': 200}
+# d2 = {'x': 300, 'y': 200}
+
+#10. Write a Python function to multiply all the items' value with 2 in a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
