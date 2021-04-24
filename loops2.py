afb91f887c4b79b95d7a33fbba7ac1cb88a9f732
Nvi Mai <nvi.mai@expogain.com>
2021-04-24 09:04:17 -0400

    new loops

---
+++ loops2.py
@@ -0,0 +1,42 @@
+# Done all following questions to have $10
+
+#Write Python functions to draw provided shapes based on the input numbers:
+
+#1. Input: n as an edge of isosceles right-angled triangle
+# ex: n = 5
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+#2. Input:  n as an edge of isosceles right-angled triangle
+# ex: n = 5
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+
+#3. Input: h as the height of the triangle
+# ex: n = 5
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+#4. Input: h as the height of the triangle
+# ex: n = 5
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+
+#5. Input: a, b as the edges of the rectangle
+#ex: a = 4, b = 5
+# * * * * *
+# *       *
+# *       *
+# * * * * *
