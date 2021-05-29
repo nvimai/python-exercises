b15b451c4e34e5986334c05b0a6b2f4dc61b215c
Nvi Mai <nvi.mai@expogain.com>
2021-05-29 10:47:35 -0400

    loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+#1. Write a Python function to reverse the order of the items from an array
+# array1 = [1, 2, 3, 4, 5, 6, 7]
+
+#2. Write a Python function to sort an array by ascending or descending
+# array = [13, 24, 366, 1, 2,]
+
+#3. Write a Python function to return the appearance of a specified element in an array
+
+#5. Write a Python function to remove all the occurrences of a specified item from an array
+
+#6. Write a Python function to sum all the items in an array of numbers
