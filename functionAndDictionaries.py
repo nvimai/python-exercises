4f5e2c6a19c8bf508ec308caf93c443c6d677951
Nvi Mai <nvi.mai@expogain.com>
2021-06-05 11:28:46 -0400

    functions and dictionaries

---
+++ functionAndDictionaries.py
@@ -0,0 +1,36 @@
+# Bython Basic 2 40k
+
+# 1. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them. 
+
+# 2. Write a Python function to input a number, if it is not a number generate an error message, or is positive, negative or zero. 
+
+# 3. Write a Python function to parse a string to Float or Integer. Name the function as "string2Float" and "string2Int"
+
+# 4. Write a Python function to sum of two given (input) integers. However, if the sum is between 15 to 20 it will return 20. 
+
+# 5. Write a Python function to convert seconds to day, hour, minutes and seconds. Function name "seconds2Datetime"
+
+# 6. Write a Python function to swap two variables. Function name "swap2Vars"
+
+# 7. Write a Python function to convert true to 1 and false to 0. 
+
+# 8. Write a Python function to clear the screen or terminal.
+
+# 9. Write a Python function to return the appearance of a specified element in an array
+
+# 10. Write a Python function to sort a dictionary on value
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+
+# 11. Write a Python function to sum all the items' value from a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+# 12. Write a Python function to get the maximum and minimum value in a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
+
+# 13. Write a Python function to merge 2 dictionaries
+# d1 = {'a': 100, 'b': 200}
+# d2 = {'x': 300, 'y': 200}
+
+# 14. Write a Python function to multiply all the items' value with 2 in a dictionary
+# dictionary = {1: 40, 2: 20, 3: 30, 4: 10, 5: 60, 6: 50}
