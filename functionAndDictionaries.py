cfb34f96dc663089c992356fe7027b61d556b478
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-06-12 11:12:35 +0700

    functionAndDictionaries.py

--- functionAndDictionaries.py
+++ functionAndDictionaries.py
@@ -1,15 +1,58 @@
 # Bython Basic 2 40k
 
-# 1. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them. 
+# 1. Write a Python function which accepts the user's first and last name and print them in reverse order with a space between them. 
+# def Name():
+#     fullName = input('Type your first name and last name: ')
+#     firstLastName = fullName.split(" ")
+#     firstName = firstLastName[0]
+#     lastName = firstLastName[1]
+#     print(f'Your name is {lastName} {firstName}')
+
+# print('Hi there')
+# Name()
+# print('Bye')
 
 # 2. Write a Python function to input a number, if it is not a number generate an error message, or is positive, negative or zero. 
+# def num():
+#     number = float(input('Type a number: '))
+#     if number > 0:
+#         print('Positive number')
+#     elif number < 0:
+#         print('Negative number')
+#     else:
+#         print('Zero')
+
+# num()
 
 # 3. Write a Python function to parse a string to Float or Integer. Name the function as "string2Float" and "string2Int"
+def parse(string2Float, string2Int):
+    print(int(string2Float))
+    print(string2Int)
 
+
+print(parse(4124.132, 114.31))
 # 4. Write a Python function to sum of two given (input) integers. However, if the sum is between 15 to 20 it will return 20. 
+# def sum(x, y):
+#     sum = x + y
+#     if sum in range(15, 20):
+#         return 20
+#     else:
+#         return sum
 
-# 5. Write a Python function to convert seconds to day, hour, minutes and seconds. Function name "seconds2Datetime"
+# print(sum(int(input('x: ')), int(input('y: '))))
+# print(sum(int(input('x: ')), int(input('y: '))))
+# print(sum(int(input('x: ')), int(input('y: '))))
+# print(sum(int(input('x: ')), int(input('y: '))))
+# 5. Write a Python function to convert seconds to day, hour, minutes. Function name "seconds2Datetime"
+seconds = 86401
+secondsInDay = 60 * 60 * 24
+secondsInHour = 60 * 60
+secondsInMinute = 60
 
+days = seconds // secondsInDay
+hours = (seconds - (days * secondsInDay)) // secondsInHour
+minutes = (seconds - (days * secondsInDay) - (hours * secondsInHour)) // secondsInMinute
+print(f'{days}: {hours}: {minutes}')
 # 6. Write a Python function to swap two variables. Function name "swap2Vars"
 
 # 7. Write a Python function to convert true to 1 and false to 0. 
--- loops.py
+++ loops.py
@@ -6,41 +6,41 @@ print(array1)
 
 #2. Write a Python function to sort an array by ascending or descending
 
-isAsc = input("ASC or DESC? ")
-array = [13, 24, 366, 1, 2,]
-def sortArray(_array, _isAsc):
+# isAsc = input("ASC or DESC? ")
+# array = [13, 24, 366, 1, 2,]
+# def sortArray(_array, _isAsc):
     
-    if _isAsc.lower() == "asc":
-        # Ascending => asc
-        _array.sort()
-        return _array
-    else:
-        # Descending => desc
-        _array.sort(reverse= True)
-        return _array
+#     if _isAsc.lower() == "asc":
+#         # Ascending => asc
+#         _array.sort()
+#         return _array
+#     else:
+#         # Descending => desc
+#         _array.sort(reverse= True)
+#         return _array
         
 
-isAsc = "asc"
-print(sortArray(array, isAsc))
-#3. Write a Python function to return the appearance of a specified element in an array
-array3 = [3, 7, 2, 1, 8, 3, 9, 2, 1]
-numberCount = int(input("Type number you want to count: "))
-count = array3.count(numberCount)
-# Ternary Operator
-s = 's' if count > 1 else ''
-print(f"This number appears {count} time{s}. And it is {count} time{s}")
+# isAsc = "asc"
+# print(sortArray(array, isAsc))
+# #3. Write a Python function to return the appearance of a specified element in an array
+# array3 = [3, 7, 2, 1, 8, 3, 9, 2, 1]
+# numberCount = int(input("Type number you want to count: "))
+# count = array3.count(numberCount)
+# # Ternary Operator
+# s = 's' if count > 1 else ''
+# print(f"This number appears {count} time{s}. And it is {count} time{s}")
 
-#5. Write a Python function to remove all the occurrences of a specified item from an array
-array4 = [2, 4, 7, 5, 8, 3, 2, 1, 2]
-numberRemove = int(input("Type number you want to remove: "))
-while array4.count(numberRemove) > 0:
-    array4.remove(numberRemove)
-print(array4)
+# #5. Write a Python function to remove all the occurrences of a specified item from an array
+# array4 = [2, 4, 7, 5, 8, 3, 2, 1, 2]
+# numberRemove = int(input("Type number you want to remove: "))
+# while array4.count(numberRemove) > 0:
+#     array4.remove(numberRemove)
+# print(array4)
 
 
-#6. Write a Python function to sum all the items in an array of numbers
-array5 = [2, 7, 5, 9, 3, 4, -18, -20, 2.5, 3.9]
-print(sum(array5))
+# #6. Write a Python function to sum all the items in an array of numbers
+# array5 = [2, 7, 5, 9, 3, 4, -18, -20, 2.5, 3.9]
+# print(sum(array5))
 
 
 
