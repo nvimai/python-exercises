674a17d918c573bc758fcadff7cf5663d2cef947
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-08-07 19:02:26 +0700

    done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Lấy hết toàn bộ dữ liệu mình cần dựa trên cấu trúc class product name, type, brand, price
-# Tìm cách lưu ra file csv 
-# beautifulsoup read URL(search web)
+# Tìm cách lưu ra file csv
+# beautifulsoup read URL
+#
 
 
 from bs4 import BeautifulSoup
@@ -11,16 +12,13 @@ with open('airpodreal.html', 'r') as htmlFile:
     nameTag = soup.find_all('h1')
     for name in nameTag:
         print(f'Name: {name.text}')
-    
-    typeTag = soup.find_all('a', {"class":"breadcrumb-item"})
+
+    typeTag = soup.find_all('a', {"class": "breadcrumb-item"})
     print(f'Type: {typeTag[2].text}')
 
     brandTag = soup.find_all('td')
     print(f'Brand: {brandTag[1].text}')
 
-    priceTag = soup.find_all('span', {"class":"product-price__current-price"})
+    priceTag = soup.find_all('span', {"class": "product-price__current-price"})
     for price in priceTag:
         print(f'Price: {price.text}')
-
-
-
